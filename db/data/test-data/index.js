29ca2bd9052bb16c0a7437e79751644974a33b76
Alexander Trout <alexandertrout@A1.local>
2020-01-20 11:56:44 +0000

    project setup, index files and requiring data for the correct environment

---
+++ db/data/test-data/index.js
@@ -0,0 +1,6 @@
+const topicData = require("./topics");
+const articleData = require("./articles");
+const commentData = require("./comments");
+const userData = require("./users");
+
+module.exports = { topicData, articleData, commentData, userData };
