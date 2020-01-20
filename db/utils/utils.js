e612202162f153d8d76322a002e513aee0d56e8f
Alexander Trout <alexandertrout@A1.local>
2020-01-20 14:39:53 +0000

    formatDates utility function passing all tests

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,15 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+  let newList = list
+    .map(obj => {
+      return { ...obj };
+    })
+    .map(newObj => {
+      let newTimestamp = new Date(newObj.created_at * 1000);
+      newObj.created_at = newTimestamp;
+      return newObj;
+    });
+  return newList;
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,49 @@
-const { expect } = require('chai');
+const { expect } = require("chai");
 const {
   formatDates,
   makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+  formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+  // This utility function should be able to take an array (list) of objects and return a new array. Each item in the new array must have its timestamp converted into a Javascript date object. Everything else in each item must be maintained.
+  it("if passed an empty array return an empty array", () => {
+    let list = [];
+    expect(formatDates(list)).to.deep.equal([]);
+  });
+  it("can convert one object in the array to have the correct timestamp", () => {
+    let list = [{ created_at: 1511354163389 }];
+    let test = formatDates(list);
+    expect(test).to.deep.equal([
+      { created_at: new Date(1511354163389 * 1000) }
+    ]);
+  });
+  it("can convert multiple objects in an array to have the correct timestamp", () => {
+    let list = [
+      { created_at: 1511354163389 },
+      { created_at: 1511354163600 },
+      { created_at: 1511354168000 }
+    ];
+    let test = formatDates(list);
+    expect(test).to.deep.equal([
+      { created_at: new Date(1511354163389 * 1000) },
+      { created_at: new Date(1511354163600 * 1000) },
+      { created_at: new Date(1511354168000 * 1000) }
+    ]);
+  });
+  it("returns a different reference in memory, and does not mutate the original list array", () => {
+    let list = [
+      { created_at: 1511354163389 },
+      { created_at: 1511354163600 },
+      { created_at: 1511354168000 }
+    ];
+    let test = formatDates(list);
+    expect(test).to.not.equal(list);
+    expect(test[0].created_at).to.not.equal(list[0].created_at);
+    expect(list).to.deep.equal(list);
+  });
+});
 
-describe('makeRefObj', () => {});
+describe("makeRefObj", () => {});
 
-describe('formatComments', () => {});
+describe("formatComments", () => {});
